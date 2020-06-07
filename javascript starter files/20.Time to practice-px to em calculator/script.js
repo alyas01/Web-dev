7534eda5e82774e24ade58c7f42d59f90aca66fe
alyas01 <misha.alyas01@gmail.com>
2020-06-07 17:30:15 -0400

    Created a PX to EM calculator using JS functions and new js page

--- javascript starter files/20.Time to practice-px to em calculator/script.js
+++ javascript starter files/20.Time to practice-px to em calculator/script.js
@@ -1,1 +1,17 @@
-// em = required / parent 
+//em = required / parent; 
+
+var calculate = () => {
+	var parentValue = document.getElementById('parent').value; 
+	var requireValue = document.getElementById('required').value; 
+	var result = document.getElementById('result');  
+	if(!parentValue | !requireValue){
+		alert("Please fill in all fields"); 
+	}else{
+		result.innerHTML = (requireValue / parentValue);
+	}
+}
+
+	//adding event listner to query selector
+
+var btn = document.querySelector('button'); 
+btn.addEventListener('click', calculate); 
